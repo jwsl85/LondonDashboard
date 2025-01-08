5c3a3867a0a30c5643ef8d60eb87bcaa2946f82f
Andrew Hughes-Onslow <kalpaitch@users.noreply.github.com>
2025-01-08 21:32:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// <script src="https://unpkg.com/@deck.gl/core@latest/dist.min.js"></script>
+// <script src="https://unpkg.com/@deck.gl/google-maps@latest/dist.min.js"></script>
+const {TileLayer, GoogleMapsOverlay} = deck;
+
 let map;
 
 async function initMap() {
@@ -7,6 +11,19 @@ async function initMap() {
     center: { lat: 51.50736618041992, lng: -0.1276291161775589 },
     zoom: 4,
   });
+
+  const apiKey = 'AlzaSyDTXnZvPceTcpvifeGsntZVY-UY5zu7LPKU';
+  const heatmapType = 'GBR_DEFRA'
+  const deckOverlay = new GoogleMapsOverlay({
+    layers: [
+      new TileLayer({
+        id: 'heatmap-tiles',
+        data: 'https://airquality.googleapis.com/v1/mapTypes/'+ heatmapType +'/heatmapTiles/{z}/{x}/{y}?key=' + apiKey,
+      })
+    ],
+  });
+
+  deckOverlay.setMap(map);
 }
 
 initMap();
